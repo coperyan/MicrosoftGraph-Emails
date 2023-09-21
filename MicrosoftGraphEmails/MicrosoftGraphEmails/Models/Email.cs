75861ee8979ecc71a5e3e75bba52fdfa1b1daf4c
Ryan Cope <rcope@1800radiator.com>
2023-09-21 16:51:03 -0700

    Initial commit

---
+++ MicrosoftGraphEmails/MicrosoftGraphEmails/Models/Email.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrosoftGraphEmails.Models
+{
+    public class Email
+    {
+        public string mailbox { get; set; }
+        public List<string> to_recipients { get; set; }
+
+        public List<string> cc_recipients { get; set; }
+
+        public List<string> bcc_recipients { get; set; }
+
+        public string subject { get; set; }
+
+        public string body { get; set; }
+
+        public List<string> file_attachments { get; set; }
+
+        public string importance { get; set; }
+    }
+}
---
+++ MicrosoftGraphEmails/MicrosoftGraphEmails/Services/MicrosoftGraphEmailService.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Graph;
+using MicrosoftGraphEmails.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrosoftGraphEmails.Services
+{
+    public class MicrosoftGraphEmailService
+    {
+        private readonly IConfiguration _config;
+
+        public MicrosoftGraphEmailService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendAsync(Email email)
+        {
+
+        }
+    }
+
+}
